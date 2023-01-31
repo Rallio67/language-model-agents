064c2dfdddb04fb4526caacaa4fb2a768ccf98b7
rallio <121454712+Rallio67@users.noreply.github.com>
2023-01-30 23:29:21 -0500

    Delete hello_there.py

--- hello_there.py
+++
@@ -1,9 +0,0 @@
-import random
-
-# This function will print "Hello world" a random number of times.
-
-def hello_there():
-  number=random.randint(0,100)
-  string = "Hello there."
-  for i in range(0,number):
-    print(string)
